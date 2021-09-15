dde90ea0089c139f92ddac5eebb11ad010f937e1
muthu <mkandanmk7@gmail.com>
2021-09-15 14:55:51 +0530

    crud-router

--- src/component/UserList.js
+++ src/component/UserList.js
@@ -4,9 +4,16 @@ import { ListGroup, ListGroupItem, Button } from "reactstrap";
 
 export const UserList = () => {
   return (
-    <ListGroup>
-      <ListGroupItem>UserOne</ListGroupItem>
-      <div className="ml-auto"></div>
+    <ListGroup className="mt-4">
+      <ListGroupItem className="d-flex">
+        UserOne
+        <div className="ml-auto">
+          <Link to="/edit" className="btn btn-primary m-2">
+            Edit
+          </Link>
+          <Button color="danger">Delete</Button>
+        </div>
+      </ListGroupItem>
     </ListGroup>
   );
 };
